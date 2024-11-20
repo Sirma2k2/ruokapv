044171a94816530ac3ca3deacf41572e2a49645f
Arttu <Ironicmanx>
2024-11-20 18:09:23 +0200

    korjaus

--- App.js
+++ App.js
@@ -1,41 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'; 
-import { createNativeStackNavigator } from '@react-navigation/native-stack'; // not needed since we are using TapBar as navigation
-import FoodScreen from './frontend/screens/foodScreen';// not needed since we are using TapBar as navigation
+//import { createNativeStackNavigator } from '@react-navigation/native-stack'; // not needed since we are using TapBar as navigation
+//import FoodScreen from './frontend/screens/foodScreen';// not needed since we are using TapBar as navigation
 import TapBar from './frontend/components/TapBar';
 import TaskBar from './frontend/components/taskBar';
 import { Platform } from 'react-native';
-import HomeScreen from './frontend/screens/HomeScreen'; // not needed since we are using TapBar as navigation
+//import HomeScreen from './frontend/screens/HomeScreen'; // not needed since we are using TapBar as navigation
 import { ThemeProvider, useTheme } from './frontend/components/ThemeContext'; // if dark mode is implemented
-import useAuth from './frontend/hooks/useAuth'; // AUTHENTICATION
+// import useAuth from './frontend/hooks/useAuth'; // AUTHENTICATION
 import WelcomeScreen from './frontend/screens/WelcomeScreen'; 
 
 
 
 export default function App() {
 
-  const { isLoggedIn, isFirstLaunch, login } = useAuth();
+//   const { isLoggedIn, isFirstLaunch, login } = useAuth();
 
 
-  if (isLoggedIn === null || isFirstLaunch === null) {
-    // Show a loading screen or splash while checking auth status
-    return null;  // Or return a loading screen component
-}
-
+//   if (isLoggedIn === true || isFirstLaunch === null) {
+//     // Show a loading screen or splash while checking auth status
+//     return null;  // Or return a loading screen component
+// } 
+ 
   return (
-
-    <NavigationContainer>
-            {isLoggedIn ? (
-                <ThemeProvider>
-                    {Platform.OS === 'android' && <TaskBar />}
-                    <TapBar />
-                </ThemeProvider>
-            ) : (
-                <WelcomeScreen onLogin={login} />
-            )}
-     </NavigationContainer>
-
+    <ThemeProvider>
+      <NavigationContainer>
+        <ThemeProvider>
+          {Platform.OS === 'android' && <TaskBar />}
+          <TapBar />
+        </ThemeProvider>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
