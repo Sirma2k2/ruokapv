3deb0f9984b7956ec74770686c7ce8554493f3fd
Sirma2k2 <miro.pennanen@gmail.com>
2024-12-01 16:51:19 +0200

    MealsSCreeniä muokattu semi kovaa fr

--- frontend/screens/MealsScreen.js
+++ frontend/screens/MealsScreen.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, Modal, Alert } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet, Modal, TextInput } from 'react-native';
 import { useTheme } from '../components/ThemeContext'; // Import the useTheme hook
 import { ActivityIndicator, Searchbar } from 'react-native-paper';
 import { FlatList } from 'react-native';
 import * as Animatable from 'react-native-animatable';
+import { Picker } from '@react-native-picker/picker';
 
 const MealsScreen = () => {
   const { theme } = useTheme(); // Access the theme from context
-  const [searchBreakfast, setSearchBreakfast] = useState('');
+  const [searchMeals, setSearchMeals] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedFood, setSelectedFood] = useState(null);
   const [foodResults, setFoodResults] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
+  const [category, setCategory] = useState('Food'); 
 
   const searchFood = async (query) => {
     if (!query) return;
@@ -20,44 +22,73 @@ const MealsScreen = () => {
     setError('');
 
     try {
-      const response = await fetch(`http://localhost:3000/api/searchFood?query=${query}`)
+      const response = await fetch(`http://localhost:3000/api/searchFood?query=${query}`);
       if (response.ok) {
-        const data = await response.json();
+        const data = await response.json()
         setFoodResults(data);
       } else {
-        setError('No products found');
+        setError('No products found')
       }
     } catch (error) {
-      setError('Failed to fetch food data');
+      setError('Failed to fetch food data')
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
 
   useEffect(() => {
-    if (searchBreakfast.length > 2) {
-      searchFood(searchBreakfast);
+    if (searchMeals.length > 2) {
+      searchFood(searchMeals)
     } else {
-      setFoodResults([]);
+      setFoodResults([])
     }
-  }, [searchBreakfast]);
+  }, [searchMeals])
 
   const saveFoodMeal = (food) => {
     console.log('Uusi ateria luotu:', food);
-    setModalVisible(false);
-  };
+    setModalVisible(false)
+  }
+
+  const saveMeal = (food) => {
+    console.log('Uusi ateria luotu:', food)
+    setModalVisible(false)
+  }
 
   return (
     <View style={{ flex: 1, padding: 16 }}>
       <Text style={[styles.header, { color: theme.text.color }]}>Create a Meal</Text>
 
-      {/* Search bar */}
-      <Searchbar
-        placeholder="Search meals"
-        onChangeText={setSearchBreakfast}
-        value={searchBreakfast}
+      <Text style={[styles.label, theme.text]}>Meal name</Text>
+      <TextInput
+        style={[styles.input, theme.input]}
+        placeholder="Enter meal name"
+        theme={{ colors: { background: '#e0e0e0' } }}
       />
 
+      {/* Category filter */}
+      <View style={styles.categorys}>
+        <Text style={[styles.categoryLabel, { color: theme.text.color }]}>Category:</Text>
+        <Picker
+          selectedValue={category}
+          onValueChange={(itemValue) => setCategory(itemValue)}
+          style={styles.pickerStyle}
+          mode="dropdown" 
+        >
+          <Picker.Item label="Food" value="Food" />
+          <Picker.Item label="Drink" value="Drink" />
+          <Picker.Item label="Salad" value="Salad" />
+          <Picker.Item label="Other" value="Other" />
+        </Picker>
+      </View>
+
+      {/* ds */}
+    
+        <Searchbar
+          placeholder={`Search for ${category}s`}
+          onChangeText={setSearchMeals}
+          value={searchMeals}
+        />
+    
       {/* Loading indicator */}
       {loading && <ActivityIndicator size="large" color="#ff0" />}
 
@@ -65,19 +96,20 @@ const MealsScreen = () => {
       {error && <Text style={{ color: 'red' }}>{error}</Text>}
 
       {/* Search results */}
-      <FlatList
-        data={foodResults}
-        keyExtractor={(item, index) => index.toString()}
-        renderItem={({ item }) => (
-          <Animatable.View animation="fadeIn" duration={400}>
-            <TouchableOpacity
-              style={styles.foodItem}
-              onPress={() => {
-                setSelectedFood(item);
-                setModalVisible(true);
-              }}
-            >
-              <Text style={{ color: theme.text.color }}>
+      {category !== 'All' && (
+        <FlatList
+          data={foodResults}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Animatable.View animation="fadeIn" duration={400}>
+              <TouchableOpacity
+                style={styles.foodItem}
+                onPress={() => {
+                  setSelectedFood(item);
+                  setModalVisible(true);
+                }}
+              >
+                <Text style={{ color: theme.text.color }}>
                 {item.product_name || 'No name'}
               </Text>
               <Text style={{ color: theme.text.color }}>
@@ -98,10 +130,11 @@ const MealsScreen = () => {
                 <Text style={{ color: theme.text.color }}>
                   Fat: {item.nutriments?.fat_100g || 'N/a'} g
                 </Text>
-            </TouchableOpacity>
-          </Animatable.View>
-        )}
-      />
+              </TouchableOpacity>
+            </Animatable.View>
+          )}
+        />
+      )}
 
       {/* Modal */}
       <Modal
@@ -140,7 +173,7 @@ const MealsScreen = () => {
                   style={styles.button}
                   onPress={() => saveFoodMeal(selectedFood)}
                 >
-                  <Text>Save food</Text>
+                  <Text>Save food to meal</Text>
                 </TouchableOpacity>
               </>
             ) : (
@@ -155,9 +188,17 @@ const MealsScreen = () => {
           </View>
         </View>
       </Modal>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => saveMeal(selectedFood)}
+      >
+        <Text>Save meal</Text>
+      </TouchableOpacity>
+
     </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -208,6 +249,21 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     width: '90%',
   },
+  label: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    color: '#333',
+  },
+  input: {
+    fontSize: 16,
+    padding: 10,
+    marginBottom: 20,
+    backgroundColor: '#f9f9f9',
+    borderColor: '#b0b0b0',
+    borderWidth: 1,
+    borderRadius: 8,
+  },
   button: {
     alignItems: 'center',
     justifyContent: 'center',
@@ -255,7 +311,37 @@ const styles = StyleSheet.create({
   mealText: {
     fontSize: 16,
     color: '#333',
-  }
+  },
+  categorys: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 10,
+    paddingHorizontal: 10,
+    backgroundColor: '#f9f9f9', 
+    borderRadius: 8,
+    borderWidth: 1, 
+    borderColor: '#ccc', 
+    shadowColor: '#000', 
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 2, 
+  },
+  categoryLabel: {
+    fontSize: 16,
+    marginRight: 10,
+    fontWeight: 'bold', 
+    color: '#333', 
+  },
+  pickerStyle: {
+    flex: 1,
+    height: 50, 
+    color: '#333',
+    fontSize: 16, 
+    backgroundColor: '#fff', 
+    borderRadius: 8, 
+    paddingHorizontal: 8, 
+  },
 });
 
 export default MealsScreen;
